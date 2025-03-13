3207e1374d5de80020f191f19b4e476c32d8e645
jackson36067 <jacksonn36067@gmail.com>
2025-03-13 13:23:28 +0800

    栏目数据添加到Redis

--- jackson-mall-common/src/main/java/com/jackson/constant/RedisConstant.java
+++ jackson-mall-common/src/main/java/com/jackson/constant/RedisConstant.java
@@ -5,4 +5,6 @@ public class RedisConstant {
     public static final Long SHOP_CATEGORY_EXPIRE_TIME = 2L;
     public static final String SHOP_EMAIL_CODE_PREFIX = "shop:code:";
     public static final Long CODE_EXPIRE_TIME = 2L;
+    public static final String SHOP_COLUMN_KEY = "shop:column";
+    public static final Long SHOP_COLUMN_EXPIRE_TIME = 2L;
 }
--- jackson-mall-system/src/main/java/com/jackson/service/impl/ColumnServiceImpl.java
+++ jackson-mall-system/src/main/java/com/jackson/service/impl/ColumnServiceImpl.java
@@ -1,7 +1,9 @@
 package com.jackson.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.json.JSONUtil;
 import com.jackson.constant.ColumnConstant;
+import com.jackson.constant.RedisConstant;
 import com.jackson.entity.ShopColumn;
 import com.jackson.repository.ColumnRepository;
 import com.jackson.repository.GoodsRepository;
@@ -10,11 +12,14 @@ import com.jackson.service.ColumnService;
 import com.jackson.vo.ColumnDetailVO;
 import com.jackson.vo.ColumnHomeVO;
 import com.jackson.vo.GoodsMessageVO;
+import io.netty.util.internal.StringUtil;
 import jakarta.annotation.Resource;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class ColumnServiceImpl implements ColumnService {
@@ -22,7 +27,7 @@ public class ColumnServiceImpl implements ColumnService {
     @Resource
     private ColumnRepository columnRepository;
     @Resource
-    private GoodsRepository goodsRepository;
+    private StringRedisTemplate stringRedisTemplate;
 
     /**
      * 获取首页栏列表
@@ -30,11 +35,23 @@ public class ColumnServiceImpl implements ColumnService {
      * @return
      */
     public Result<List<ColumnHomeVO>> getColumnList() {
-        List<ColumnHomeVO> columnHomeVOList = columnRepository
-                .findAll(Sort.by(Sort.Direction.ASC, ColumnConstant.ORDER_COLUMN))
-                .stream()
-                .map(shopColumn -> BeanUtil.copyProperties(shopColumn, ColumnHomeVO.class))
-                .toList();
+        // 从Redis中获取栏目数据
+        String shopColumnJsonList = stringRedisTemplate.opsForValue().get(RedisConstant.SHOP_COLUMN_KEY);
+        List<ColumnHomeVO> columnHomeVOList = List.of();
+        // 判断Redis中是否有数据
+        if (StringUtil.isNullOrEmpty(shopColumnJsonList)) {
+            // 没有 -> 从数据库中获取栏目数据
+            columnHomeVOList = columnRepository
+                    .findAll(Sort.by(Sort.Direction.ASC, ColumnConstant.ORDER_COLUMN))
+                    .stream()
+                    .map(shopColumn -> BeanUtil.copyProperties(shopColumn, ColumnHomeVO.class))
+                    .toList();
+            // 将栏目数据保存到Redis中
+            stringRedisTemplate.opsForValue().set(RedisConstant.SHOP_COLUMN_KEY, JSONUtil.toJsonStr(columnHomeVOList), RedisConstant.SHOP_COLUMN_EXPIRE_TIME, TimeUnit.DAYS);
+        } else {
+            // 有 -> 直接从Redis中获取数据
+            columnHomeVOList = JSONUtil.toList(shopColumnJsonList,ColumnHomeVO.class);
+        }
         return Result.success(columnHomeVOList);
     }
 
